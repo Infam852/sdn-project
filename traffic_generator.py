35af3481159d15d29b714cceea71b1ef4e5bba9d
None <None>
2021-11-27 09:40:52 -0800

    traffic generator wrapped in function

--- traffic_generator.py
+++ traffic_generator.py
@@ -3,23 +3,31 @@ import os
 import time
 import random
 
-while True:
-    elephant = True if random.randint(1,20) == 1 else False
-    
-    if elephant:
-        flow_duration = round(exp(scale=20), 2)
-        bandwidth = round(1024 ** 2 * exp(scale=20))  # Mb/s
-    else:
-        flow_duration = round(exp(scale=1), 2)
-        bandwidth = round(1024 ** 2 * exp(scale=0.1))  # Mb/s
-    
-    time_to_next_flow = round(exp(scale=0.05), 3)  # s
-    
 
-    port = random.randint(61001, 64000)
+def generator(elephant_duration_lambda=20, elephant_bandwidth_lambda=20, non_elephant_duration_lambda=1,
+              non_elephant_bandwidth_lambda=0.1, time_between_flows_lambda=0.05, port_min=61001, port_max=64000,
+              ip_dest='10.0.0.2'):
+    while True:
+        elephant = True if random.randint(1, 20) == 1 else False
 
-    time.sleep(time_to_next_flow)
-    command = "iperf -c 10.0.0.2 -u -b {} -t {} -p {} &".format(bandwidth, flow_duration, port)
-    print('------------------------------------------------------')
-    print("Executing command: ", command)
-    os.system(command)
+        if elephant:
+            flow_duration = round(exp(scale=elephant_duration_lambda), 2)
+            bandwidth = round(1024 ** 2 * exp(scale=elephant_bandwidth_lambda))  # Mb/s
+        else:
+            flow_duration = round(exp(scale=non_elephant_duration_lambda), 2)
+            bandwidth = round(1024 ** 2 * exp(scale=non_elephant_bandwidth_lambda))  # Mb/s
+
+        time_to_next_flow = round(exp(scale=time_between_flows_lambda), 3)  # s
+
+        port = random.randint(port_min, port_max)
+
+        time.sleep(time_to_next_flow)
+        command = "iperf -c {} -u -b {} -t {} -p {} &".format(ip_dest, bandwidth, flow_duration, port)
+        print('------------------------------------------------------------')
+        print("Executing command: ", command)
+        print('------------------------------------------------------------')
+        os.system(command)
+
+
+if __name__ == '__main__':
+    generator()
